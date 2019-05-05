8301b5e51a44244c2eb37abea0537c3f8f8bfca0
Daniel Emod Kovacs <kovacsemod@gmail.com>
2019-05-05 11:56:37 +0200

    chore(ci): revert to using default next build and serve
    The config was getting messy so I cleaned it up in favour of a future release of Next and Now supporting serving icons from `/static` at root.

--- next.config.js
+++ next.config.js
@@ -3,25 +3,36 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const withFonts = require('next-fonts');
 const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
+const {
+  readFileSync
+} = require('fs');
 
-const { DEPLOYMENT_TARGET } = process.env;
+const getCommitHash = () => {
+  const rev = readFileSync('.git/HEAD').toString();
+  if (rev.indexOf(':') === -1) {
+    return rev;
+  } else {
+    return readFileSync('.git/' + rev.substring(5)).toString();
+  }
+}
 
-const config = withCSS({
-  target: DEPLOYMENT_TARGET,
+const config = {
+  target: 'serverless',
+  generateBuildId: async () => {
+    return getCommitHash();
+  },
   webpack(config, options) {
     config.module.rules.push({
       test: /\.tsx?$/,
-      use: [
-        {
-          loader: 'linaria/loader',
-          options: {
-            sourceMap: process.env.NODE_ENV !== 'production',
-          },
+      use: [{
+        loader: 'linaria/loader',
+        options: {
+          sourceMap: process.env.NODE_ENV !== 'production',
         },
-      ],
+      }, ],
     });
     return config;
   },
-});
+}
 
-module.exports = withImages(withFonts(withTypescript(config)));
+module.exports = withImages(withFonts(withTypescript(withCSS(config))));
