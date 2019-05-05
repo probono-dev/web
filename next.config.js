de4dbefe31883339ce0d8579d07e330eff44ed51
Daniel Emod Kovacs <kovacsemod@gmail.com>
2019-05-05 12:00:25 +0200

    chore(ci): remove unnecessary build ID generation
    It was not in the scope of this.

--- next.config.js
+++ next.config.js
@@ -3,24 +3,9 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const withFonts = require('next-fonts');
 const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
-const {
-  readFileSync
-} = require('fs');
-
-const getCommitHash = () => {
-  const rev = readFileSync('.git/HEAD').toString();
-  if (rev.indexOf(':') === -1) {
-    return rev;
-  } else {
-    return readFileSync('.git/' + rev.substring(5)).toString();
-  }
-}
 
 const config = {
   target: 'serverless',
-  generateBuildId: async () => {
-    return getCommitHash();
-  },
   webpack(config, options) {
     config.module.rules.push({
       test: /\.tsx?$/,
